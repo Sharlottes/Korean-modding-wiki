916e788a890a50176d1918168571c9d851d199df
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-08-27 19:18:59 +0900

    i believe gdeaf
    Signed-off-by: sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>

---
+++ scripts/xylene.js
@@ -0,0 +1,15 @@
+const xylene = extendContent(Liquid, "xylene", {
+  effect: extendContent(StatusEffect, "xylened", {
+    damageMultiplier: 1,
+    armorMultiplier: 0.9,
+    speedMultiplier: 0.7,
+    effect: Fx.shieldBreak
+  }),
+  color: Color.valueOf("cfa580"),
+  barColor: Color.valueOf("cfa580"),
+  flammability: 0.75,
+  explosiveness: 0.1,
+  temperature: 0.5,
+  heatCapacity: 0,
+  viscosity: 0.2
+});
